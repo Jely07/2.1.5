4afdfa3fac0907b67226d1e7275a8953407ae445
Shegay Anzhelika <jely91@icloud.com>
2023-09-12 11:23:12 +0900

    Commit2

--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class Wood3 {
 
-    private final Rabbit4 rabbit;
+    private Rabbit4 rabbit;
 
+    public Rabbit4 getRabbit() {
+        return rabbit;
+    }
     @Autowired
-    public Wood3(Rabbit4 rabbit) {
+    public void setRabbit(Rabbit4 rabbit) {
         this.rabbit = rabbit;
     }
 
